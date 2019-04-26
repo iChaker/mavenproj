110e62d8ef50543f0418018ef153934f3deed620
iChaker <chaker.iyadh@gmail.com>
2019-04-26 11:02:30 +0100

    init

---
+++ src/main/java/com/mycompany/mavenproject1/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Iyadh
+ */
+public class Hello {
+    
+    public static void main(String[ ] args)
+    {
+    
+    
+    System.out.println("Hello NGAAA");
+    System.out.println("Hello NGAAA");
+    }
+       
+}
